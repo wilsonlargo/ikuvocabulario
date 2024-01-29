427a2f11e1ec072bbe6ddb65af1470fcf4d6447e
Wilson Largo <136187461+wilsonlargo@users.noreply.github.com>
2024-01-29 13:48:39 -0500

    a

--- src/comandos.js
+++ src/comandos.js
@@ -1,4 +1,5 @@
 var OrderAZ;
+var idioma;
 function VernacularAZ(a, b) {
     // Use toUpperCase() to ignore character casing
     const bandA = a.lx.toUpperCase();
@@ -14,20 +15,19 @@ function VernacularAZ(a, b) {
 }
 
 function orderVerAZ() {
-
-
+    idioma=="loc"
     OrderAZ = 1;
     vervocabulario();
 }
 
 function orderVerZA() {
-
+    idioma=="loc"
     OrderAZ = -1;
     vervocabulario();
 }
 
 function vervocabulario() {
-
+    idioma="loc"
     let PalabrasSort = palabras.sort(VernacularAZ);
     const elemento = document.getElementById("contenedorPalabras");
     elemento.innerHTML = "";
@@ -38,9 +38,9 @@ function vervocabulario() {
         nuevaPalabra.innerHTML = `
             <div class="mb-1">
             <div class="row">
-                <div class="col-auto me-aut fw-bold">${palabra.lx}</div>
-                <div class="col-auto me-aut text-success">${palabra.ps}</div>
-                <div class="col-auto me-aut fst-italic ms-3">${palabra.dn}</div>
+                <div class="col-auto me-aut fw-bold">${palabra.lx.toLowerCase()}</div>
+                <div class="col-auto me-aut text-success">${palabra.ps.toLowerCase()}</div>
+                <div class="col-auto me-aut fst-italic ms-3">${palabra.dn.toLowerCase()}</div>
             </div>
             </div>      `
         elemento.appendChild(nuevaPalabra)
@@ -64,18 +64,19 @@ function NationalAZ(a, b) {
 
 
 function orderNatAZ() {
-
+    idioma="es"
     OrderAZ = 1;
     vervocabularioNat();
 }
 
 function orderNatZA() {
+    idioma=="es"
     OrderAZ = -1;
     vervocabularioNat();
 }
 
 function vervocabularioNat() {
-
+    idioma="es"
     let PalabrasSort = palabras.sort(NationalAZ);
     const elemento = document.getElementById("contenedorPalabras");
     elemento.innerHTML = "";
@@ -86,8 +87,8 @@ function vervocabularioNat() {
         nuevaPalabra.innerHTML = `
             <div class="mb-1">
             <div class="row">
-                <div class="col-auto me-aut fw-bold ms-3">${palabra.dn}</div>
-                <div class="col-auto me-aut fst-italic">${palabra.lx}</div>
+                <div class="col-auto me-aut fw-bold">${palabra.dn.toLowerCase()}</div>
+                <div class="col-auto me-aut fst-italic ms-3">${palabra.lx.toLowerCase()}</div>
             </div>
             </div>      `
         elemento.appendChild(nuevaPalabra)
@@ -97,22 +98,41 @@ function vervocabularioNat() {
 async function filtrar(Leter) {
     const elemento = document.getElementById("contenedorPalabras");
     elemento.innerHTML = "";
+    let filtrado;
+    if (idioma=="es") {
+        filtrado = palabras.filter(
+            (palabra) => palabra.dn.startsWith(Leter)
+        );
+        filtrado.forEach(palabra => {
+            //Creamos uncontenedor por palabra
+            const nuevaPalabra = document.createElement('div');
+            nuevaPalabra.innerHTML = `
+                <div class="mb-1">
+                <div class="row">                   
+                    <div class="col-auto me-aut fst-italic">${palabra.dn}</div>
+                    <div class="col-auto me-aut fw-bold ms-3">${palabra.lx}</div>
+                </div>
+                </div>      `
+            elemento.appendChild(nuevaPalabra)
+        })
+    } else if (idioma="loc") {
+        filtrado = palabras.filter(
+            (palabra) => palabra.lx.startsWith(Leter)
+        );
+        filtrado.forEach(palabra => {
+            //Creamos uncontenedor por palabra
+            const nuevaPalabra = document.createElement('div');
+            nuevaPalabra.innerHTML = `
+                <div class="mb-1">
+                <div class="row">
+                    <div class="col-auto me-aut fw-bold">${palabra.lx}</div>
+                    <div class="col-auto me-aut text-success">${palabra.ps}</div>
+                    <div class="col-auto me-aut fst-italic ms-3">${palabra.dn}</div>
+                </div>
+                </div>      `
+            elemento.appendChild(nuevaPalabra)
+        })
+    }
 
-    const filtrado = palabras.filter(
-        (palabra) => palabra.lx.startsWith(Leter)
-    );
 
-    filtrado.forEach(palabra => {
-        //Creamos uncontenedor por palabra
-        const nuevaPalabra = document.createElement('div');
-        nuevaPalabra.innerHTML = `
-            <div class="mb-1">
-            <div class="row">
-                <div class="col-auto me-aut fw-bold">${palabra.lx}</div>
-                <div class="col-auto me-aut text-success">${palabra.ps}</div>
-                <div class="col-auto me-aut fst-italic ms-3">${palabra.dn}</div>
-            </div>
-            </div>      `
-        elemento.appendChild(nuevaPalabra)
-    })
 }
